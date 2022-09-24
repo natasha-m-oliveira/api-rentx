0ee2fd34e2f32eaf2e1c2cd4e671924eb2594b8a
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-09-24 18:03:21 -0300

    feat: configured the database

---
+++ src/database/index.ts
@@ -0,0 +1,19 @@
+import { createConnection, getConnectionOptions } from "typeorm";
+
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions()
+  .then((options) => {
+    const newOptions = options as IOptions;
+    // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+    newOptions.host = "database";
+
+    void createConnection({
+      ...options,
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
