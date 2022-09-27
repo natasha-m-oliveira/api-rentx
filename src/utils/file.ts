cac77dd8efbe66b1a129938110f7f0b4d984734a
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-09-26 18:18:43 -0700

    feat: update user avatar

---
+++ src/utils/file.ts
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+export const deleteFile = async (filename: string): Promise<void> => {
+  try {
+    await fs.promises.stat(filename);
+    await fs.promises.unlink(filename);
+  } catch (err) {
+    console.log(err);
+  }
+};
