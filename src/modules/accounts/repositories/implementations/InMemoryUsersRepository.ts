1ca9e572d5824b877223ea492c5ee4d2e093ab66
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-09-27 16:30:57 -0700

    test: authenticate user

---
+++ src/modules/accounts/repositories/implementations/InMemoryUsersRepository.ts
@@ -0,0 +1,39 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  private readonly users: User[];
+
+  constructor() {
+    this.users = [];
+  }
+
+  async create({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      password,
+      driver_license,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find((user) => user.email === email);
+    return user;
+  }
+
+  async findById(id: string): Promise<User> {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+}
