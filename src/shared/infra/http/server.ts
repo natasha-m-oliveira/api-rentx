0918a370d63d774904c2ceb9458888f61ca9cffb
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-09-30 15:05:18 -0700

    test(categories): integration

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,28 +1,5 @@
-import "reflect-metadata";
-
-import express from "express";
-import "express-async-errors";
-import swaggerUi from "swagger-ui-express";
-
-import "../../container";
-
-import { errorHandler } from "@shared/infra/http/middlewares/errorHandler";
-import { router } from "@shared/infra/http/routes";
-
-import swaggerFile from "../../../swagger.json";
-import createConnection from "../typeorm";
-
-void createConnection();
+import { app } from "./app";
 
 const port = 3333;
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
