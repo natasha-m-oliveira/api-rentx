6b4afd2f44ea1cc19ab8f27128915406a90d1170
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-10-13 22:54:22 -0300

    build: error handler with sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,9 @@ import express from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import "../../container";
 
 import upload from "@config/upload";
@@ -19,9 +22,21 @@ void createConnection();
 
 const app = express();
 
-app.use(express.json());
-
 app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+app.use(express.json());
 
 app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
@@ -31,6 +46,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use("/api/v1", router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorHandler);
 
